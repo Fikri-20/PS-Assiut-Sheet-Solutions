680e7183e15788e6fe063b2b7f0f9beb52f93359
Ahmed Fikri <149339473+Fikri-20@users.noreply.github.com>
2025-02-09 06:48:11 +0200

    Update A.c

--- Sheet A/A.c
+++ Sheet A/A.c
@@ -1,1 +1,17 @@
+#include <stdio.h> 
 
+void fileIO() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+int main(){
+    fileIO();
+    char name[100]; 
+    scanf("%s", &name);
+    printf("Hello, %s", name);
+
+    
+    return 0; 
+}
